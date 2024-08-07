09c91d906871bb4ed3e50f6835a23c26fc04c264
sarakalvig <115412142+sarakalvig@users.noreply.github.com>
2024-08-07 21:19:02 +0200

    submenu header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,3 @@
-// src/components/Header.js
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css'; // Add styles if needed
@@ -8,10 +7,15 @@ const Header = () => {
     <header>
       <nav>
         <ul>
-          <li><Link to="/hellas">Hellas</Link></li>
-          <li><Link to="/france">France</Link></li>
-          <li><Link to="/japan">Japan</Link></li>
-          <li><Link to="/india">India</Link></li>
+          <li>
+            <button className="dropdown-button">Countries</button>
+            <ul className="submenu">
+              <li><Link to="/hellas">Hellas</Link></li>
+              <li><Link to="/france">France</Link></li>
+              <li><Link to="/japan">Japan</Link></li>
+              <li><Link to="/india">India</Link></li>
+            </ul>
+          </li>
           <li><Link to="/travel-diary">Travel Diary</Link></li>
           <li><Link to="/sort-and-filter">Sort and Filter</Link></li>
         </ul>
